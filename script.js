76ee67adb4de185e8d9ee28517928ca27e1c312b
Zenagrams <zadamenwu@gmail.com>
2022-12-05 19:41:56 -0800

    Create an array for quiz data containing objects. Te objects have properties with values for the question, answers and correct answer.

---
+++ script.js
@@ -0,0 +1,38 @@
+const quizData = [//an array with objects containing quiz questions, choices and answers
+    {
+        question: 'We eat breakfast.',
+        a:'Nosotros desayunamos.',
+        b:'Yo ceno.',
+        c:'Tu comes comida casera.',
+        d:'Ellos almuerzan.',
+        correct:'a'
+    },{
+        question: 'I check my email.',
+        a:'Ella ve una pelicula.',
+        b:'Yo leo un libro.',
+        c:'Yo reviso mi correo electronico.',
+        d:'Ella alimenta a su perro.',
+        correct:'c'
+    }, {
+        question: 'You get out of bed.',
+        a:'Se sienta en la silla.',
+        b:'Subo la escalera.',
+        c:'Te levantas de la cama.',
+        d:'Bajamos las escaleras.',
+        correct:'c'
+    },{
+        question: " He brushes his teeth.",
+        a:'Te peinas.',
+        b:'El se cepilla los dientes.',
+        c:'Me lavo la cara.',
+        d:'El usa hilo dental.',
+        correct:'b'
+    },{
+        question: "They go to work.",
+        a:'Van a hacer ejercicio.',
+        b:'Ella va al parque.',
+        c:'Voy a la biblioteca.',
+        d:'Ellos van a trabajar.',
+        correct:'d'
+    }
+]
